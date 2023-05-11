dc137c36ffbd37a45dce1c40e5f944e1d0d6f6f4
timghill <tim_hill_2@sfu.ca>
2023-05-11 10:44:22 -0700

    Fix bug in runme.m and tidy up

--- box-steady/runme.m
+++ box-steady/runme.m
@@ -18,35 +18,6 @@ if any(steps==2)
 
 	% Run parameterization script to set up geometry, velocity, material properties, etc.
 	md=parameterize(md,'moulin.par');
-
-% 	% HYDROLOGY SPECIFIC PARAMETERIZATION:
-% 	% Change hydrology class to Sommers' SHaKTI model
-% 	md.hydrology=hydrologyshakti();
-% 
-% 	% Define initial water head such that water pressure is 50% of ice overburden pressure
-% 	md.hydrology.head = 0.5*md.materials.rho_ice/md.materials.rho_freshwater*md.geometry.thickness + md.geometry.base;
-% 
-% 	% Initial subglacial gap height of 0.01m everywhere
-% 	md.hydrology.gap_height = 0.01*ones(md.mesh.numberofelements,1);
-% 
-% 	% Typical bed bump bump spacing (2m)
-% 	md.hydrology.bump_spacing = 2*ones(md.mesh.numberofelements,1);
-% 
-% 	% Typical bed bump height (0.1m)
-% 	md.hydrology.bump_height = 0.1*ones(md.mesh.numberofelements,1);
-% 
-% 	% Define distributed englacial input to the subglacial system (m/yr)
-% 	% Change the value 0.0 to add distributed input
-% 	md.hydrology.englacial_input = 0.0*ones(md.mesh.numberofvertices,1);
-% 
-% 	% Initial Reynolds number (start at Re=1000 everywhere)
-% 	md.hydrology.reynolds= 1000*ones(md.mesh.numberofelements,1);
-% 
-% 	% Set up atmospheric pressure Type I boundary condition at left edge of
-% 	% domain (outflow, i.e. h=zb at x=xmin)
-% 	md.hydrology.spchead = NaN(md.mesh.numberofvertices,1);
-% 	pos=find(md.mesh.vertexonboundary & md.mesh.x==min(md.mesh.x));
-% 	md.hydrology.spchead(pos)=md.geometry.base(pos);
     
     % GLADS HYDROLOGY PARAMETERIZATION
     md.hydrology=hydrologyglads();
@@ -58,18 +29,16 @@ if any(steps==2)
     md.hydrology.melt_flag = 1;
 
     md.hydrology.ischannels = 1;
-    md.hydrology.channel_conductivity = 0.05*ones(md.mesh.numberofvertices, 1);
+    md.hydrology.channel_conductivity = 0.05;%*ones(md.mesh.numberofvertices, 1);
 
     md.hydrology.spcphi = NaN(md.mesh.numberofvertices,1);
 	pos=find(md.mesh.vertexonboundary & md.mesh.x==min(md.mesh.x));
 	md.hydrology.spcphi(pos)=0;
     
     ic_head = md.materials.rho_ice/md.materials.rho_freshwater*md.geometry.thickness + md.geometry.base;
-	md.initialization.watercolumn = 0.5*md.hydrology.bump_height.*ones(md.mesh.numberofvertices, 1);
+	md.initialization.watercolumn = 0.1*md.hydrology.bump_height.*ones(md.mesh.numberofvertices, 1);
     md.initialization.hydraulic_potential = md.constants.g*md.materials.rho_freshwater*md.geometry.base;
     md.initialization.channelarea = 0.01*ones(md.mesh.numberofedges, 1);
-% 	md.hydrology.head = 0.5*md.materials.rho_ice/md.materials.rho_freshwater*md.geometry.thickness + md.geometry.base;
-
 
     md.basalforcings.groundedice_melting_rate = 0.05*ones(md.mesh.numberofvertices, 1);
     md.basalforcings.geothermalflux = 50;
@@ -99,13 +68,14 @@ if any(steps==3)
 	% %Add one moulin with steady input at x=500, y=500
 	% [a,pos] = min(sqrt((md.mesh.x-500).^2+(md.mesh.y-500).^2));
 % 	time=0:md.timestepping.time_step:md.timestepping.final_time;
-	md.hydrology.moulin_input = zeros(md.mesh.numberofvertices, 1);
 	% md.hydrology.moulin_input(pos,:)=4;
+    
+    % Zero moulin inputs
+	md.hydrology.moulin_input = zeros(md.mesh.numberofvertices, 1);
 
 	% Specify no-flux Type 2 boundary conditions on all edges (except
 	% the Type 1 condition set at the outflow above)
 	md.hydrology.neumannflux=zeros(md.mesh.numberofelements,1);
-% 	md.hydrology.neumannflux(end,:)=time;
 
     md.friction.coefficient = zeros(md.mesh.numberofelements, 1);
     md.friction.p = 1;
@@ -116,8 +86,6 @@ if any(steps==3)
     md.initialization.vy = zeros(md.mesh.numberofvertices, 1);
     md.miscellaneous.name = 'projectname_ISSM_run_01';
     md = setmask(md,'','');
-
-%     md.friction.coeffient
 
 	md.verbose.solution=1;
 	md=solve(md,'Transient');
